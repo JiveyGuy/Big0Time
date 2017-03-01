eefacf2f900b519e76434e16b3aafd958649ff19
JiveyGuy <jason.ivey.not.micheal@gmail.com>
2017-03-01 00:22:08 -0700

    Added tester

--- Functions.java
+++ Functions.java
@@ -1,7 +1,10 @@
 public class Functions{
+	public static final boolean DEBUG = true;
 
-	public int funcOne(int a[],int n)
+	public int funcOne(int a[])
 	{
+		if(DEBUG){System.out.println("funcOne Called for array length: " + a.length);}
+		int n = a.length;
   		int i,j,k;
   		int sum,maxSum = 0;
   		for(i=0; i<n; i++)
@@ -20,8 +23,10 @@ public class Functions{
     return maxSum;
 	}
 
-	public int funcTwo(int a[],int n )
+	public int funcTwo(int a[])
 	{
+		if(DEBUG){System.out.println("funcTwo Called for array length: " + a.length);}
+		int n = a.length;
     	int i,j,sum,maxSum;
     	maxSum = 0;
     	for(i = 0;i<n;i++)
@@ -36,8 +41,10 @@ public class Functions{
     	}
     	return maxSum;
 	}
-	public int funcThree(int a[], int n)
+	public int funcThree(int a[])
 	{
+		if(DEBUG){System.out.println("funcThree Called for array length: " + a.length);}
+		int n = a.length;
    		int maxSum = 0,sum = 0;
    		int i;
    		for(i = 0;i<n;i++)
--- Timer.java
+++ Timer.java
@@ -1,17 +1,21 @@
 public class Timer{
-	
+	public static final boolean DEBUG = true;
 	private Double start;
 
 	public Timer(){
+		if(DEBUG){System.out.println("instance of Timer made");}
 		start = new Double(System.currentTimeMillis());
 	}
 
 	public void setTime(){
+		if(DEBUG){System.out.println("Time|Set");}
 		start = new Double(System.currentTimeMillis());
 	}
 
 	public Double getTime(){
-		return start;
+		double currentTime = start - System.currentTimeMillis();
+		if(DEBUG){System.out.println("Time returned = " + currentTime);}
+		return currentTime;
 	}
 
 }
