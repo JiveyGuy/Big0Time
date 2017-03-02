8874b4d09f62135dbddf4d5ae6f83e30c85c9b7f
Jason Ivey <jason.ivey.not.micheal@gmail.com>
2017-03-01 17:33:20 -0700

    Update FunctionTester.java

--- FunctionTester.java
+++ FunctionTester.java
@@ -22,61 +22,24 @@ public class FunctionTester{
 
 		Timer timer   = new Timer();
 		Double time[] = new Double[3];
-
-		time[0] = new Double(0.0);
-		time[1] = new Double(0.0);
-		time[2] = new Double(0.0); //Our time holders to sum time
-
-		
+	
 
 		for(int testsCompleted = 0; testsCompleted < 10; testsCompleted++){
 
-			PecanTree bruteTree = new PecanTree();
-			PecanTree quadTree = new PecanTree();
-			PecanTree kTree = new PecanTree();
+
 
 			for(int iterations = 0; iterations < size; iterations++){
 
-				timer.setTime();
-				Functions.funcOne(array);
-				bruteTree.bud(timer.getTime());
 
-				timer.setTime();
-				Functions.funcTwo(array);
-				quadTree.bud(timer.getTime());
-
-				timer.setTime();
-				Functions.funcThree(array);
-				kTree.bud(timer.getTime());
 
 			} 
-			Double values[][] = new Double[3][3];
 
-			values[0] = populateValues(bruteTree);
-			values[1] = populateValues(quadTree);
-			values[2] = populateValues(kTree);
-			System.out.println("For dataset size = " + size + " and number of tests " + tests);
-			for(int x = 0; x < values.length; x++){
-				System.out.print("Values["+x+"] = ");
-				for (int y = 0; y < values[x].length; y++){
-					System.out.print("["+y+"] = " + values[x][y]);
-				} System.out.println(); //Clearing a line
-			}
+
+
 			size = size * rate; //Increasing by the rate
 			array = randomGen(size); //New array huray! 
 		}
 	}
-
-	public static Double[] populateValues(PecanTree pecanTree){
-		Double[] values = new Double[3];
-
-		values[0] = pecanTree.getMin();
-		values[1] = pecanTree.getAverage();
-		values[2] = pecanTree.getMax();
-		
-		return values;
-	}
-
 	public static int[] randomGen(int size){
 
     	if (DEBUG) System.out.println("Random gen = " + size + " ");
