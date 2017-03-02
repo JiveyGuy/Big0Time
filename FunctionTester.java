b7ebed911d9c819ea410ee359d85b9432f438f4e
Jason Ivey <jason.ivey.not.micheal@gmail.com>
2017-03-01 18:35:51 -0700

    Update FunctionTester.java

--- FunctionTester.java
+++ FunctionTester.java
@@ -5,6 +5,7 @@
 	the Timer object.
 */
 import java.util.Random;
+import java.util.PriorityQueue;
 
 public class FunctionTester{
 
@@ -12,8 +13,11 @@ public class FunctionTester{
 	public static void main(String args[]){
 
 		if (DEBUG) System.out.println("Called for:  " + args[0] + " : " + args[1]);
-
-
+		
+		PriorityQueue<Double> brute = new PriorityQueue<Double>();
+		PriorityQueue<Double> quad = new PriorityQueue<Double>();
+		PriorityQueue<Double> kaine = new PriorityQueue<Double>();
+		
 		int size      = Integer.parseInt(args[0]); //Size of array
 		int tests     = Integer.parseInt(args[1]); //# of tests to run
 		int rate      = 2;
@@ -22,19 +26,29 @@ public class FunctionTester{
 
 		Timer timer   = new Timer();
 		Double time[] = new Double[3];
-	
+
 
 		for(int testsCompleted = 0; testsCompleted < 10; testsCompleted++){
 
-
+	
 
 			for(int iterations = 0; iterations < size; iterations++){
-
-
+			
+				timer.setTime();
+				Functions.funcOne();
+				brute.add(timer.getTime());
+				
+				timer.setTime();
+				Functions.funcTwo();
+				quad.add(timer.getTime());
+				
+				timer.setTime();
+				Functions.funcThree();
+				kaine.add(timer.getTime());
 
 			} 
 
-
+			
 
 			size = size * rate; //Increasing by the rate
 			array = randomGen(size); //New array huray! 
@@ -52,6 +66,12 @@ public class FunctionTester{
      		 if (DEBUG) System.out.println("Random Loop " + iterations + " : " + result[iterations]);
     
     	}
+	
+	public void Print(int func, PriorityQueue<Double> input){
+		Double[] array = input.toArray();
+		System.out.println(For func #" + func + " lowest = " + array[0] + " highest
+		
+	}
     
     	return result; 
   	}
